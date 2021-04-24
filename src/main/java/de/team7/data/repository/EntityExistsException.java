d06ec6a17998d8ebaa6df1c285948b252f6e4f66
Vincent Nadoll <vincent.nadoll@googlemail.com>
2021-04-24 15:57:48 +0200

    Add low-level API for accessing entities

---
+++ src/main/java/de/team7/data/repository/EntityExistsException.java
@@ -0,0 +1,16 @@
+package de.team7.data.repository;
+
+import lombok.NoArgsConstructor;
+
+/**
+ * This exception is thrown in case an entity exists where none expected.
+ *
+ * @author Vincent Nadoll
+ */
+@NoArgsConstructor
+public class EntityExistsException extends RuntimeException {
+
+    public EntityExistsException(String message) {
+        super(message);
+    }
+}
